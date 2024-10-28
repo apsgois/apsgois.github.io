01739a47bd9b7283c6c69e1d5e36900b302bac9d
ana <desenvolvimento10@volt.ind.br>
2024-10-28 16:31:01 -0300

    Atualizando a doc

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "api-controlador-de-carga-MPPT-60A.yml",
+    url: "api-ups-dc-smart-620.yml",
     dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
